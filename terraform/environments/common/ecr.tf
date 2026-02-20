7975eedb90c46ea56828d58f0e8e42aa090bb271
Fergal Kearns <159787545+fergalhk@users.noreply.github.com>
2026-02-20 15:59:40 +0000

    feat: move common terraform to management repo (#25)

--- terraform/environments/common/ecr.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_ecr_repository" "test_app" {
-  force_delete         = true
-  image_tag_mutability = "MUTABLE"
-  name                 = var.app_name
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-}
